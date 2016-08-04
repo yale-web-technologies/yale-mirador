1d86216673098663d0f236aa6bb17865447fa945
Seong-June Kim <seong-june.kim@yale.edu>
2016-08-04 10:00:28 -0400

    Comments.

--- src/js/annotation/parsed-annotations.js
+++ src/js/annotation/parsed-annotations.js
@@ -1,27 +1,64 @@
 import getMiradorWindow from '../mirador-window';
 import annoUtil from './anno-util';
 
-export default class {
+/**
+ * Builds a tree-like structure (annoHiercrchy) of annotations
+ * so they can be accessed and manipulated
+ * according to the pre-defined TOC tags hierarchy (tagHierarchy).
+ */
+export default class ParsedAnnotations {
   constructor(annotations) {
     this.annotations = annotations;
-    this.tagHierarchy = [];  // array of arrays
+
+    /*
+     * tagHierarchy is a JSON passed from outside (an array of arrays).
+     * It defines the tags to be used to define the hiearchy.
+     * It is different from "ranges" because 
+     * it is used to define a strucutre of annotations in a single canvas 
+     * while ranges are used to define a structure of canvases in a sequence.
+     * For example, the first array could list tags for sections of a story
+     * and the second one could list tags for sub-sections.
+     */
+    this.tagHierarchy = [];
+    
     this.tagWeights = {}; // for sorting
+    
+    /**
+     * This can be considered the output of parse,
+     * while "this.tagHierarchy" and "annotations" are the input.
+     * 
+     * Each node is an object:
+     * {
+     *   label: A_STRING, // label for display
+     *   tag: A_STRING, // value of the tag
+     *   weight: A_NUMBER, // for sorting
+     *   annotation: AN_OBJECT, // annotation
+     *   childNodes: AN_OBJECT, // child TOC nodes as a hashmap on tags
+     *   childAnnotations: AN_ARRAY, // non-TOC-node annotations that targets this node
+     *   isRoot: A_BOOL, // true if the node is the root
+     * }
+     */
     this.annoHierarchy = null;
+    
     this.annoToNodeMap = {}; // key: annotation ID, value: node in annoHierarchy;
     this.init();
   }
   
   init(annotations) {
     this.tagHierarchy = getMiradorWindow().getConfig().extension.tagHierarchy;
+   
     console.log('ParsedAnnotations#init tagHierarchy: ');
     console.dir(this.tagHierarchy);
     
-    this.annoHierarchy = this.newNode(null, true);
+    this.annoHierarchy = this.newNode(null, true); // root node
     
     this.initTagWeights();
     this.parse(this.annotations);
   }
   
+  /**
+   * Assign weights to tags according to their position in the array.
+   */
   initTagWeights() {
     var _this = this;
     jQuery.each(this.tagHierarchy, function(rowIndex, row) {
@@ -38,6 +75,10 @@ export default class {
     this.addRemainingAnnotations(remainingAnnotations);
   }
   
+  /**
+   * Build a TOC structure
+   * @return An array of annotations that are NOT assigned to a TOC node.
+   */
   addTaggedAnnotations(annotations) {
     var _this = this;
     var remainder = [];
@@ -68,7 +109,11 @@ export default class {
     });
   }
   
-  // rowIndex: index of this.annoHierarchy
+  /**
+   * Recursively builds the TOC structure.
+   * @param {rowIndex} index of this.annoHierarchy
+   * @return true if the annotation was set to be a TOC node, false if not.
+   */
   buildChildNodes(annotation, tags, rowIndex, parent) {
     //console.log('ParsedAnnotations#buildNode rowIndex: ' + rowIndex + ', anno:');
     //console.dir(annotation);
@@ -76,10 +121,10 @@ export default class {
     var tagHierarchy = this.tagHierarchy;
     var currentNode = null;
 
-    if (rowIndex >= tagHierarchy.length) {
-      if (parent.isRoot) {
+    if (rowIndex >= tagHierarchy.length) { // no more levels to explore in the TOC structure
+      if (parent.isRoot) { // The root is not a TOC node
         return false;
-      } else {
+      } else { // Assign the annotation to parent (a TOC node)
         parent.annotation = annotation;
         this.annoToNodeMap[annotation['@id']] = parent;
         return true;
@@ -110,6 +155,13 @@ export default class {
     }
   }
   
+  /**
+   * A tag object is an object in this.tagHierarcy that represents a tag.
+   *
+   * @param {tags} Array of tags
+   * @param {tagObjectList} Array of tag objects
+   * @return The tag object if one of the objects in tagObjectList represents one of the tags; null if not.
+   */
   tagInList(tags, tagObjectList) {
     var match = null;
     jQuery.each(tags, function(index, tag) {
