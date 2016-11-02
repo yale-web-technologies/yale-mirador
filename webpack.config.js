51d756b21e36049cc06ebea7c5d70dbdaea5f0b2
Seong-June Kim <seong-june.kim@yale.edu>
2016-11-02 10:31:14 -0400

    Added babel preset for es2017 to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015']
+        presets: ['es2015', 'es2017']
       }
     }, {
       test: /\.less$/,
