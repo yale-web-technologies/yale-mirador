09e35af932d1b9071c4b450b5146c81224546c61
Seong-June Kim <seong-june.kim@yale.edu>
2017-01-30 09:26:43 -0500

    Print log when subscribed callback is called

--- src/js/util/override-pubsub.js
+++ src/js/util/override-pubsub.js
@@ -1,25 +1,46 @@
 /**
- * Overrides jQuery tiny pubsub code to print traces.
+ * Replaces/overrides jquery-tiny-pubsub code to print traces.
  * Include this file for debugging only! 
  */ 
 (function($) {
 
+  // Add strings or patterns to this array and
+  // event names
+  var excludePatterns = [];
+  var exclude = function(str) {
+    for (var i = 0; i < excludePatterns.length; ++i) {
+      if (str.match(excludePatterns[i])) {
+        return true;
+      }
+    }
+    return false;
+  };
   var o = $({});
 
   $.subscribe = function() {
-    console.log('SUB ' + Array.prototype.slice.call(arguments));
+    var args = Array.prototype.slice.call(arguments);
+    console.log('SUB', args);
+    var eventType = args[0];
+    var callback = args[1];
+    args[1] = function() {
+      if (!exclude(eventType)) {
+        console.log('ACK ', eventType, Array.prototype.slice.call(arguments, 1));
+      }
+      callback.apply(null, arguments);
+    };
     o.on.apply(o, arguments);
   };
 
   $.unsubscribe = function() {
-    console.log('UNS ' + Array.prototype.slice.call(arguments));
+    console.log('UNSUB', Array.prototype.slice.call(arguments));
     o.off.apply(o, arguments);
   };
 
   $.publish = function() {
     var args = Array.prototype.slice.call(arguments);
-    if (!args[0].match('updateTooltips.')) {
-      console.log('PUB ' + args);
+    var eventType = args[0];
+    if (!exclude(eventType)) {
+      console.log('PUB', args);
     }
     o.trigger.apply(o, arguments);
   };
