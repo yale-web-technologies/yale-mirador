cab00b9f79c17993ad6ab4a155fd7491d2377d5a
Seong-June Kim <seong-june.kim@yale.edu>
2017-02-07 16:43:33 -0500

    Print version info to console

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -3,7 +3,7 @@ import getLogger from './util/logger';
 
 // Separated this code out to its own file because it shouldn't run with the test.
 jQuery(document).ready(function() {
-  getLogger().debug('Yale Mirador Extension document ready');
+  getLogger().info(window._YaleMiradorVersion);
   if (jQuery('#ym_grid').length > 0) {
     const app = new App({
       rootElement: 'ym_grid',
