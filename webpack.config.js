9b19629921347fe08bd8d3fb536c629ee5f91dd1
Seong-June Kim <seong-june.kim@yale.edu>
2017-02-07 16:29:11 -0500

    Adding an extra line to header
    to circumvent Drupal stripping comment lines

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,16 @@ var BannerWebpackPlugin = require('banner-webpack-plugin');
 var path = require('path');
 var npmPackage = require('./package');
 
+// The extra line added is a kludge to circumvent Drupal stripping comment lines
 function header() {
-  return '// Yale-Mirador version ' + npmPackage.version + ' - ' + new Date() + '\n\n';
+  var text = 'Yale-Mirador version ' + npmPackage.version + ' - ' + new Date();
+  return '// ' + text + '\nwindow._YaleMiradorVersion="' + text + '";\n\n';
 }
 module.exports = {
   debug: false,
-  entry: { 
+  entry: {
     'yale-mirador': [
-      './src/js/app.js', 
+      './src/js/app.js',
       './src/js/bootstrap.js'
     ]
   },
