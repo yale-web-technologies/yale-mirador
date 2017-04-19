5056bcaca19352bd15d0142a12d021ea9e10fab3
Seong-June Kim <seong-june.kim@yale.edu>
2017-04-19 16:58:36 -0400

    Update shape boundaries as mouse moves
    https://github.com/yale-web-technologies/mirador-project/issues/113

--- src/js/extension/ext-osd-region-draw-tool.js
+++ src/js/extension/ext-osd-region-draw-tool.js
@@ -1,9 +1,9 @@
 (function($) {
-  
+
   $.yaleExt = $.yaleExt || {};
-  
+
   /*
-   * All functions in this file must be called in the context of 
+   * All functions in this file must be called in the context of
    * an OsdRegionDrawTool so "this" will point to the instance of it.
    */
   jQuery.extend($.yaleExt, {
@@ -22,7 +22,7 @@
       });
       return out_shapes;
     },
-    
+
     /*
      * Highlight annotated area for annotation focused in annotation window.
      */
@@ -39,8 +39,25 @@
         });
       });
       this.osdViewer.forceRedraw();
+    },
+
+    updateHighlightsMulti: function(annotations) {
+      for(let shapes of Object.values(this.annotationsToShapesMap)) {
+        for (let shape of shapes) {
+          for (let annotation of annotations) {
+            if (shape.data.annotation['@id'] === annotation['@id']) {
+              $.yaleExt.highlightShape(shape);
+              shape.bringToFront();
+            } else {
+              $.yaleExt.deHighlightShape(shape);
+              shape.sendToBack();
+            }
+          }
+        }
+      }
+      //this.osdViewer.forceRedraw();
     }
-    
+
   });
-  
+
 })(Mirador);
